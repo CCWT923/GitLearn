80ca991bb175dac58e7008343c76e041fdd9bfe9
WuTao <ccwutao@gmail.com>
2018-09-01 22:49:13 +0800

    分离宾客档案和开房记录

--- GuestInfo.sql
+++ GuestInfo.sql
@@ -1,3 +1,4 @@
+--宾客档案信息
 SELECT
 	g.name '姓名',
 	g.birth '生日',
@@ -6,27 +7,17 @@ SELECT
 	g.sex '性别',
 	g.mobile '手机',
 	g.phone '电话',
-	g.fv_date '首次入住',--第一次消费日期
-	g.fv_room '首次入住房号',--第一次房号
-	g.fv_rate '首次房费',--第一次房价
-	g.lv_date,--最后一次日期
-	g.lv_room,--最后一次房号
-	g.lv_rate,--最后一次房价
-	g.i_times,--入住次数
-	g.x_times,--取消次数
-	g.n_times,--nowshow次数
-	g.i_days,--房晚
-	h.roomno,
-	h.type,
-	h.sta,
-	h.arr,
-	h.dep,
-	h.ratecode,
-	h.setrate,
-	h.ref
+	g.fv_date '首次入住',
+	g.fv_room '首次入住房号',
+	g.fv_rate '首次房费',
+	g.lv_date '最后一次入住',
+	g.lv_room '最后一次房号',
+	g.lv_rate '最后一次房费',
+	g.i_times '入住次数',
+	g.x_times '取消次数',
+	g.n_times 'NoShow次数',
+	g.i_days '房晚',
 from
-	guest g, hmaster h
-where
-	h.haccnt = g.no
-order by
-	g.name ASC;
+	guest g
+	
+--开房记录
